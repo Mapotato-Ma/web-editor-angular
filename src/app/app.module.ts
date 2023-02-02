c22ca3298579c98c6734bdfcb3310cbc9d91f4c6
mapf <mapf@tiansu-china.com>
2023-02-02 09:09:28 +0800

    feat(0.0.0):修改布局

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
+import { MainLeftComponent } from './main/main-left/main-left.component';
+import { MainRightComponent } from './main/main-right/main-right.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainComponent
+    MainComponent,
+    MainLeftComponent,
+    MainRightComponent
   ],
   imports: [
     BrowserModule,
